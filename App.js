d1f9314cb94f707e1c0f6c118527a18f9b7539db
Sahil Shaikh <sahilshaikh1634@gmail.com>
2023-06-03 03:37:05 +0530

    4th session

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "title"}, "Namaste World!");
+// React.createElement => Object => HTML(DOM)
 
-const heading2 = React.createElement("h2", { id: "title"}, "from React");
+// const heading1 = React.createElement("h1", { id: "title", key: "h1"} , "Namaste World!");
 
-const container = React.createElement("div", { id : "container"}, [heading1, heading2]);
+// const heading2 = React.createElement("h2", { id: "title", key:"h2"}, "from React");
+
+
+// JSX => React.createElement => Object => HTML(DOM)
+
+// This is not HTML inside in Javascript but it has HTML like syntax which is used in javascript. 
+// 
+
+
+// React Component 
+// - Name of Component start with capital letter. (recommeded)
+// - 
+// - Functional Component => New
+// - Class base Component => OLD
+
+const Title = () => <h1 id="title" key='h1'>Namaste React ..!</h1>
+
+const HeaderComponent = () =>(
+        <div id="container">
+            <Title/>
+            <h1 id="title" key='h2'> Hello It a React Functional Component..</h1>
+            <h2 id="title" key='h3'>Hii</h2>
+        </div>
+    );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeaderComponent/>);
